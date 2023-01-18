c827119576188e88131f8a957ff9e90d4aa73b52
ruchitha36 <122854121+ruchitha36@users.noreply.github.com>
2023-01-18 10:01:11 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,46 @@
+"""
+Day:Tuesday
+Date & Time:10th Jan 2023
+Topic: Printing Hello World
+Description: To print hello world in different ways.
+"""
+# print('Hello World')
+# print("Hello World")
+# print('''Hello World''')              #firstway
+# print("""Hello World""")
+
+
+# print('Hell\'o World')
+# print("Hell'o World")
+# print('''Hell'o World''')            #secondway
+# print("""Hell'o World""")
+
+# print('Hello Wor"ld')
+# print("Hello Wor\"ld")
+# print('''Hello Wor"ld''')            #thirdway
+# print("""Hello Wor"ld""")
+
+# print('print(hello world)')
+# print("print(hello world)")
+# print('''print(hello world)''')       #fourthway
+# print("""print(hello world)""")
+
+
+# print('print(he\'llo world)')
+# print("print(he'llo world)")
+# print('''print(he'llo world)''')       #fifthway
+# print("""print(he'llo world)""")
+
+
+# print('''HELLO
+# WORLD
+# ''')                                   #sixthway
+
+# print('''HELLO
+# WORLD''')
+
+# print('''HELLO
+# WORLD''')
+
+# print("""HELLO
+# WORLD""")
